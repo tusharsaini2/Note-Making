366f54cd0f33ba95b9b0968d5a7cddab985f42f0
tusharsaini2 <hellotushar111@gmail.com>
2024-08-21 22:32:15 +0530

    Add style sheet

---
+++ script.js
@@ -0,0 +1,47 @@
+let noteContainer = document.querySelector(".notes-container");
+let button = document.querySelector(".btn");
+let inputText = document.querySelectorAll(".input-text");
+
+
+function updateNote () {
+  localStorage.setItem("note",noteContainer.innerHTML)
+  localStorage.clear;
+}
+
+function showNote (){
+  noteContainer.innerHTML = localStorage.getItem("note");
+  
+}
+
+showNote();
+
+button.addEventListener("click", () => {
+  console.log("helo")
+  let paragraph = document.createElement("P");
+  let img = document.createElement("IMG");
+  paragraph.setAttribute("contenteditable","true")
+  paragraph.className= "input-text";
+  img.src ="Images/Delete.png"
+  img.classList.add("img")
+
+  noteContainer.appendChild(paragraph).appendChild(img);
+});
+
+noteContainer.addEventListener("click", function(e){
+  if(e.target.tagName ==="IMG"){
+    e.target.parentElement.remove()
+    updateNote();
+  }
+  else{
+    let notes = document.querySelectorAll(".input-text");
+    notes.forEach(nt =>{
+      nt.onkeyup = function(){
+        updateNote();
+      }
+    })
+  }
+
+  
+
+})
+
